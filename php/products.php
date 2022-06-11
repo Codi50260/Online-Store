0cf5220e256c34ae9b7c7305b6ac120d74f33421
Codi <codi@trixta.com>
2022-06-11 15:32:30 +0200

    Rename API

---
+++ php/products.php
@@ -0,0 +1,22 @@
+<?php
+ 
+require "../vendor/autoload.php";
+
+$app = new Slim\App();
+
+$app->get("/products",function(){
+    require_once 'connect.php';// Calling the database connection filerequire_once 'db.php';// Calling the database connection file
+
+    $query = "select * from products";// SQL query
+    $result = $conn->query($query);
+
+    while ($row = $result->fetch_assoc()){// Loop through each field in the library table
+        $data[] = $row;// Store each field in an array
+    }
+    
+    return json_encode($data);// Translate this array into JSON
+});
+
+$app->run();
+
+?>
