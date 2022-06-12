a95c26f1ab802f8ac3188da93b98a4c8d2e20c9d
Codi <codi@trixta.com>
2022-06-12 14:09:36 +0200

    Added error reading

--- php/post_individual_product.php
+++ php/post_individual_product.php
@@ -21,12 +21,11 @@ if ($result->num_rows > 0) {
     $image3 = $product["product_img_3"];
 
     $sql2 = "UPDATE individual_product SET product_name = '$name', product_price = $price, product_rating = $rating, product_total_reviews = $review, category = '$category', desc1 = '$desc1', desc2 = '$desc2', desc3 = '$desc3', image_path_1 = '$image1', image_path_2 = '$image2', image_path_3 = '$image3'";
-    $conn->query($sql2);
 }
 
 if ($conn->query($sql2) === TRUE) {
     header('Location: '."../product_info.html");
 } else {
-    echo "An unexpected error has occured. Please try again";
+    echo "An unexpected error has occured. Please try again" . $conn->error;
 }
 ?>
