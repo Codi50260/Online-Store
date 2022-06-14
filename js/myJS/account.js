6660830e6a57fb9586ef4e6a1a4863b8d500da8e
Codi <codi@trixta.com>
2022-06-14 19:51:03 +0200

    Added user details onto page

--- js/myJS/account.js
+++ js/myJS/account.js
@@ -21,7 +21,83 @@ function check(){
         const password = document.getElementById('password').value;
 
         if (user.user_email == email && user.user_password == password){
-            location.href = "home.html";
+            document.getElementById("form_submit").submit();
+        }
+    }
+}
+
+function checkName(){
+    fetch("http://localhost/HelloWorld/Online-Store/php/users")
+        .then((response) => {
+            if (response.ok) {
+            return response.json();
+            } else {
+            throw new Error("NETWORK RESPONSE ERROR");
+            }
+        })
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                name(data, i);
+            }
+        })
+        .catch((error) => console.error("FETCH ERROR:", error));
+
+    function name(data, i){
+        const user = data[i]
+
+        if (user.logged_in == "True"){
+            document.getElementById("username").innerHTML = user.user_name;
+        }
+    }
+}
+
+function signOut(){
+    fetch("http://localhost/HelloWorld/Online-Store/php/users")
+        .then((response) => {
+            if (response.ok) {
+            return response.json();
+            } else {
+            throw new Error("NETWORK RESPONSE ERROR");
+            }
+        })
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                remove(data, i);
+            }
+        })
+        .catch((error) => console.error("FETCH ERROR:", error));
+
+    function remove(data, i){
+        const user = data[i]
+
+        if (user.logged_in == "True"){
+            location.href = 'php/sign_out.php'
+        }
+    }
+}
+
+function fillDetails(){
+    fetch("http://localhost/HelloWorld/Online-Store/php/users")
+        .then((response) => {
+            if (response.ok) {
+            return response.json();
+            } else {
+            throw new Error("NETWORK RESPONSE ERROR");
+            }
+        })
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                details(data, i);
+            }
+        })
+        .catch((error) => console.error("FETCH ERROR:", error));
+
+    function details(data, i){
+        const user = data[i]
+
+        if (user.logged_in == "True"){
+            document.getElementById("name").value = user.user_name;
+            document.getElementById("email").value = user.user_email;
         }
     }
 }
